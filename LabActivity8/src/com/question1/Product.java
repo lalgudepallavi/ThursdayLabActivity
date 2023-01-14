11b16ae607a48443a04579d668e6c249fc73f9d2
lalgudepallavi <pallavilalgude76@gmail.com>
2023-01-15 05:01:57 +0530

    activity 8

---
+++ LabActivity8/src/com/question1/Product.java
@@ -0,0 +1,17 @@
+package com.question1;
+public class Product {
+
+	int id;
+	String name;
+	double price;
+	Product(int id,String name,double price){  // creating constructor
+		this.id=id;  
+		this.name=name;  
+		this.price=price; 
+	
+	
+	
+
+	}
+
+}
---
+++ LabActivity8/src/com/question2/States.java
@@ -0,0 +1,29 @@
+package com.question2;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class States {
+
+	HashSet<String> s1=new HashSet<>();
+	
+	void addState(String stateName)
+	{
+		s1.add(stateName);
+	}
+	
+	String retriveState(String stateName)
+	{
+		Iterator it=s1.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(it.next());
+			}
+if(s1.contains(stateName))
+	return stateName;
+else
+return null ;
+	}
+	
+	}
+
+
