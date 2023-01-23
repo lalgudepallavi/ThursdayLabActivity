030d0a0426dbefaa17ee3e473abf23dedba843a1
lalgudepallavi <pallavilalgude76@gmail.com>
2023-01-24 02:53:04 +0530

    activity9

---
+++ LabActivity9/src/com/program/Program3.java
@@ -0,0 +1,14 @@
+package com.program;
+public class Program3 {
+
+	public static void main(String[] args) {
+		int [] arr = new int [] {11, 12, 13, 14, 15};  
+		
+		System.out.println("Array in reverse order: ");  
+        
+        for (int i = arr.length-1; i >= 0; i--) {  
+            System.out.print(arr[i] + " ");  
+	}
+
+}
+}
